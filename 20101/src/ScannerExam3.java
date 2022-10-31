63fcbd3eef62f731a9ddd4fe49f5c2edddc6395a
Tan_san <62871665+kangjs0906@users.noreply.github.com>
2022-10-31 15:59:49 +0900

    1031

---
+++ 20101/src/ScannerExam3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ScannerExam3 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int X = scanner.nextInt();
+		scanner.nextLine();
+		double Y = scanner.nextDouble();
+			System.out.println(X+" "+Y);
+	}
+
+}
